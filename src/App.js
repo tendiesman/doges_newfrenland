3ceed1839ab1bc64128bdd7226736681f140c1e2
tendiesman <tendiesman@protonmail.com>
2022-07-15 20:36:22 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Typewriter from 'typewriter-effect'
+import logo2 from './logo.png'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={logo2} />
         <img src={'https://bdhjwyivindi2bhlnotszwj5i3vmgfs2iacouud54s5l5bdn54.arweave.net/CM-6bYRVDRo0E62unLNk9RurDFlpABOpQfeS6voRt78?ext=png'} alt="logo" />
         <p>
           <Typewriter options={{
@@ -24,8 +26,19 @@ function App() {
         >
           Journey to Newfrenland
         </a>
+
       </header>
+      <div
+  style={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}
+>
+  <div style={{flex: 1, height: '10px', backgroundColor: 'cyan'}} />
+
+  <div style={{flex: 1, height: '10px', backgroundColor: 'cyan'}} />
+</div>
+<header className="About Us">Hello cruel world</header>
+
     </div>
+    
   );
 }
 
